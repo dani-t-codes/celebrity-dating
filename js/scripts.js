9671af5dc6cc1db595b43d3fe9bff9f837a1a582
Danielle Thompson <danithompson74@gmail.com>
2020-10-22 15:53:58 -0700

    add celebrity images to html, keep trying to fix javascript, add div id styles to css page, add eventPreventDefault back to javascript
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 //business logic 
 $(document).ready(function() {
-  $("#animal").submit (fuction(event)) 
+  $("#animal").submit(fuction(event)) 
 });
 //user interface
   const million = $("select#million").val(); {
-
     if (million === "Go on a romantic holiday with my lover") {
-      $('#nick').show();
+      $('#nick').fadeIn();
     } else if (million === "Start a wildlife sanctuary") { 
-      $('#jane').show();
+      $('#jane').fadeIn();
     } else if (million === "Trek around the world") {
-      $('#bear').show();
+      $('#bear').fadeIn();
     } else (million === "Start a coding school") {
-      $('#ada').show ();
-    }  
-  };
+      $('#ada').fadeIn ();
+    } 
+    event.preventDefault();
+  });
 
-  $(#nick).text();
 
 //const dob = $("#born").val();
 // const favoriteColor = $("#color").val();
